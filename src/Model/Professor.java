7cf8254d8daf40f095b4b2f3c4644f1c9ba9ebb8
Arthur de Moura Del Esposte <arthurmde@yahoo.com.br>
2013-01-03 17:24:38 -0200

    Classe Professor

--- src/Model/Professor.java
+++ src/Model/Professor.java
@@ -1,7 +1,18 @@
 package Model;
 
+import antlr.collections.List;
+
 public class Professor {
 	public String nome;
 	public String cpf;
 
+	public Professor(String nome, String cpf){
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public static List getAll(){
+		return null;
+		
+	}
 }
