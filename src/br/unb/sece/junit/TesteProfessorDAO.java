80b0719576b4a2afdf9967c4fdd9ba3d873eabbc
cleitoncsg <cleiton.tom@hotmail.com>
2013-02-13 17:34:46 -0200

    Testes Unitários Professor (parte 1)

---
+++ src/br/unb/sece/junit/TesteProfessorDAO.java
@@ -0,0 +1,46 @@
+package br.unb.sece.junit;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.unb.sece.model.Professor;
+import br.unb.sece.model.DAO.ProfessorDAO;
+
+
+public class TesteProfessorDAO {
+
+	private ProfessorDAO professorDAO;
+	private Professor professor;
+	
+	@Before
+	public void setUp(){
+		
+		professorDAO = new ProfessorDAO();
+		professor = new Professor();
+	}
+	
+	@Test
+	public void testarInstancia(){
+		assertNotNull(professorDAO);
+	}
+	
+	@Test
+	public void testarGetAll(){
+	
+		try{
+			professorDAO.listAll(professor.getClass());
+		}
+		catch(Exception e){
+			fail("Ocorreu um erro");
+		}
+	}
+	
+	
+	public void testarGetAllErro(){
+	
+		//A fazer...
+	}
+	
+
+}
