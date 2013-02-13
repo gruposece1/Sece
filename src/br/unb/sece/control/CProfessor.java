7ad5ebb41cff819fbc7889443456ac42482f47c5
cleitoncsg <cleiton.tom@hotmail.com>
2013-02-13 17:18:47 -0200

    Testes Unitários professor (parte 1)

---
+++ src/br/unb/sece/control/CProfessor.java
@@ -0,0 +1,19 @@
+package br.unb.sece.control;
+
+public class CProfessor {
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public CProfessor(int a) {
+		super();
+		this.a = a;
+	}
+
+}
---
+++ src/br/unb/sece/view/VProfessor.java
@@ -0,0 +1,5 @@
+package br.unb.sece.view;
+
+public class VProfessor {
+
+}
