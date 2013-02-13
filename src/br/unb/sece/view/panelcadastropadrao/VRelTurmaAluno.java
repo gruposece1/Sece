efae9f0bc65ecb7621d42726bb7fd823e8eca2dc
Arthur de Moura Del Esposte <arthurmde@yahoo.com.br>
2013-02-13 17:27:13 -0200

    Cadastro de Aluno na Turma

---
+++ src/br/unb/sece/view/panelcadastropadrao/VRelTurmaAluno.java
@@ -0,0 +1,44 @@
+package br.unb.sece.view.panelcadastropadrao;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VRelTurmaAluno extends JPanel {
+
+	private JTextField textField;
+
+	/**
+	 * Create the panel.
+	 */
+	public VRelTurmaAluno()  {
+		setLayout(null);
+		
+		JLabel lblNome = new JLabel("Nome ");
+		lblNome.setBounds(10, 11, 46, 14);
+		add(lblNome);
+		
+		textField = new JTextField();
+		textField.setBounds(83, 8, 238, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		super.setBounds(20,11,522,234);
+
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
+	}
+	
+	/**
+	 * Create the panel.
+	 */
+	
+
+
+}
