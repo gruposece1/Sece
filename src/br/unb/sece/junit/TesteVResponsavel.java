a8502b144ffe65459002106831f1af2c41c6d408
cleitoncsg <cleiton.tom@hotmail.com>
2013-02-23 21:09:09 -0300

    Finalização Teste unitário Manter Responsável

---
+++ src/br/unb/sece/junit/TesteVResponsavel.java
@@ -0,0 +1,34 @@
+package br.unb.sece.junit;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.unb.sece.exceptions.BancoDeDadosException;
+import br.unb.sece.model.Responsavel;
+import br.unb.sece.view.VResponsavel;
+
+public class TesteVResponsavel {
+	
+	private VResponsavel vresponsavel;
+	private Responsavel responsavel;
+	
+	@Before
+	public void setUp() throws BancoDeDadosException{
+		vresponsavel = new VResponsavel();
+		
+		responsavel = new Responsavel();
+		responsavel.setNome("Cleiton");
+		responsavel.setCEP("72800000");
+		responsavel.setEmail("sece@gmail.com");
+		
+	}
+	
+	@Test
+	public void testarInstancia(){
+		
+		assertNotNull(vresponsavel);
+	}
+
+}
